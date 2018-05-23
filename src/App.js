6d8acb8b2f19abf80cbc61cb1b997c394b054c5b
stam <junkboy0315@gmail.com>
2018-05-23 13:10:09 +0900

    add more samples

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ class App extends Component {
   render() {
     return (
       <div className={styles}>
+        <h1><a href="https://github.com/junkboy0315/react-compare-image">React Compare Image</a></h1>
+
         <p>200px width (cat1.jpg | cat2.jpg)</p>
         <div style={{ maxWidth: '200px' }}>
           <ReactCompareImage leftImage="/cat1.jpg" rightImage="/cat2.jpg" />
@@ -34,6 +36,17 @@ class App extends Component {
         <div style={{ maxWidth: '100%', boxSizing: 'border-box' }}>
           <ReactCompareImage leftImage="/forest1.jpg" rightImage="/cat2.jpg" />
         </div>
+
+        <p>100% width (scene1.jpg | scene2.jpg)</p>
+        <div style={{ maxWidth: '100%', boxSizing: 'border-box' }}>
+          <ReactCompareImage leftImage="/scene1.jpg" rightImage="/scene2.jpg" />
+        </div>
+
+        <p>100% width (scene2.jpg | scene1.jpg)</p>
+        <div style={{ maxWidth: '100%', boxSizing: 'border-box' }}>
+          <ReactCompareImage leftImage="/scene2.jpg" rightImage="/scene1.jpg" />
+        </div>
+
       </div>
     );
   }
