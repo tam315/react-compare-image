85315c38e2cd2abc027c7f219d31babe5847be86
stam <junkboy0315@gmail.com>
2018-05-24 11:05:33 +0900

    add storybook

---
+++ stories/index.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import ReactCompareImage from '../src/ReactCompareImage';
+
+const leftImageSrc = "/cat1.jpg";
+const rightImageSrc = "/cat2.jpg";
+
+storiesOf('ReactCompareImages', module)
+  .add('200px', () => (<div style={{maxWidth: '200px', padding: '30px 0', background: 'gray'}}><ReactCompareImage leftImage={leftImageSrc} rightImage={rightImageSrc} /></div>))
+  .add('300px', () => (<div style={{maxWidth: '300px', padding: '30px 0', background: 'gray'}}><ReactCompareImage leftImage={leftImageSrc} rightImage={rightImageSrc} /></div>))
+  .add('500px', () => (<div style={{maxWidth: '500px', padding: '30px 0', background: 'gray'}}><ReactCompareImage leftImage={leftImageSrc} rightImage={rightImageSrc} /></div>))
+  .add('100%', () => ( <div style={{maxWidth: '100%', padding: '30px 0', background: 'gray'}}><ReactCompareImage leftImage={leftImageSrc} rightImage={rightImageSrc} /></div>))
+  .add('all', () => (
+    <div>
+      <div style={{maxWidth: '200px', padding: '30px 0', background: 'gray'}}><ReactCompareImage leftImage={leftImageSrc} rightImage={rightImageSrc} /></div>
+      <p />
+      <div style={{maxWidth: '300px', padding: '30px 0', background: 'gray'}}><ReactCompareImage leftImage={leftImageSrc} rightImage={rightImageSrc} /></div>
+      <p />
+      <div style={{maxWidth: '500px', padding: '30px 0', background: 'gray'}}><ReactCompareImage leftImage={leftImageSrc} rightImage={rightImageSrc} /></div>
+      <p />
+      <div style={{maxWidth: '100%', padding: '30px 0', background: 'gray'}}><ReactCompareImage leftImage={leftImageSrc} rightImage={rightImageSrc} /></div>
+    </div>
+  ))
