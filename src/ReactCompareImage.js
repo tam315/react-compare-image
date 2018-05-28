6651c1a869553c1f44e93180878a2ec7890f66d7
stam <junkboy0315@gmail.com>
2018-05-28 14:32:21 +0900

    refactoring

--- src/ReactCompareImage.js
+++ src/ReactCompareImage.js
@@ -21,9 +21,29 @@ class ReactCompareImage extends React.Component {
     };
   }
 
-  componentDidMount() {
-    const that = this;
+  componentDidMount = () => {
+    const containerElement = this.containerRef;
 
+    this.setImagesSize();
+
+    // Re-set images size when container size is changed
+    new ResizeSensor(containerElement, () => {
+      this.setImagesSize();
+    });
+
+    containerElement.addEventListener('mousedown', this.startSliding);
+    containerElement.addEventListener('touchstart', this.startSliding);
+
+    window.addEventListener('mouseup', this.finishSliding);
+    window.addEventListener('touchend', this.finishSliding);
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener('mouseup', this.finishSliding);
+    window.removeEventListener('touchend', this.finishSliding);
+  };
+
+  setImagesSize = () => {
     // Image size set as follows.
     //
     // 1. set under image size like so:
@@ -34,83 +54,55 @@ class ReactCompareImage extends React.Component {
     //     width  = 100% of container width
     //     height = under image's height
     //              (protrudes is hidden by css 'object-fit: hidden')
-    function setImagesSize() {
-      that.setState({
-        imageWidth: that.refs.container.getBoundingClientRect().width,
-      });
-    };
-
-    setImagesSize();
-
-    const containerElement = this.refs.container;
-
-    // Re-set images size when container size is changed
-    new ResizeSensor(containerElement, () => {
-      setImagesSize();
+    this.setState({
+      imageWidth: this.underImageRef.getBoundingClientRect().width,
     });
+  };
 
-    containerElement.addEventListener('mousedown', startSliding);
-    containerElement.addEventListener('touchstart', startSliding);
-
-    window.addEventListener('mouseup', finishSliding);
-    window.addEventListener('touchend', finishSliding);
-    this.finishSliding = finishSliding; // reference for clean up
-
-    function startSliding(e) {
-      // Prevent default behavior other than mobile scrolling
-      if (!('touches' in e)) {
-        e.preventDefault();
-      }
-
-      // Slide the image even if you just click or tap (not drag)
-      handleSliding(e);
-
-      window.addEventListener('mousemove', handleSliding);
-      window.addEventListener('touchmove', handleSliding);
+  startSliding = e => {
+    // Prevent default behavior other than mobile scrolling
+    if (!('touches' in e)) {
+      e.preventDefault();
     }
 
-    function finishSliding() {
-      window.removeEventListener('mousemove', handleSliding);
-      window.removeEventListener('touchmove', handleSliding);
-    }
+    // Slide the image even if you just click or tap (not drag)
+    this.handleSliding(e);
 
-    function handleSliding(e) {
-      let pos = getCursorPosFromImageLeft(e);
+    window.addEventListener('mousemove', this.handleSliding);
+    window.addEventListener('touchmove', this.handleSliding);
+  };
 
-      const minPos = 0 + that.props.sliderWidth / 2;
-      const maxPos = that.state.imageWidth - that.props.sliderWidth / 2;
+  finishSliding = () => {
+    window.removeEventListener('mousemove', this.handleSliding);
+    window.removeEventListener('touchmove', this.handleSliding);
+  };
 
-      if (pos < minPos) pos = minPos;
-      if (pos > maxPos) pos = maxPos;
+  handleSliding = event => {
+    const e = event || window.event;
 
-      that.setState({
-        sliderPositionPercentage: pos / that.state.imageWidth,
-      });
-    }
+    // Calc Cursor Position from the left edge of the viewport
+    const cursorXfromViewport = e.touches ? e.touches[0].pageX : e.pageX;
 
-    function getCursorPosFromImageLeft(event) {
-      const e = event || window.event;
+    // Calc Cursor Position from the left edge of the window (consider any page scrolling)
+    const cursorXfromWindow = cursorXfromViewport - window.pageXOffset;
 
-      // Distance from the left edge of the viewport
-      const cursorXfromViewport = e.touches ? e.touches[0].pageX : e.pageX;
+    // Calc Cursor Position from the left edge of the image
+    const imagePosition = this.underImageRef.getBoundingClientRect();
+    let pos = cursorXfromWindow - imagePosition.left;
 
-      // Distance from the left edge of the window (consider any page scrolling)
-      const cursorXfromWindow = cursorXfromViewport - window.pageXOffset;
+    // Set minimum and maximum values ​​to prevent the slider from overflowing
+    const minPos = 0 + this.props.sliderWidth / 2;
+    const maxPos = this.state.imageWidth - this.props.sliderWidth / 2;
 
-      // Distance from the left edge of the image
-      const imagePosition = that.refs.underImage.getBoundingClientRect();
-      let cursorXfromImage = cursorXfromWindow - imagePosition.left;
+    if (pos < minPos) pos = minPos;
+    if (pos > maxPos) pos = maxPos;
 
-      return cursorXfromImage;
-    }
-  }
+    this.setState({
+      sliderPositionPercentage: pos / this.state.imageWidth,
+    });
+  };
 
-  componentWillUnmount() {
-    window.removeEventListener('mouseup', this.finishSliding);
-    window.removeEventListener('touchend', this.finishSliding);
-  }
-
-  render() {
+  render = () => {
     const styles = {
       container: {
         boxSizing: 'border-box',
@@ -123,7 +115,8 @@ class ReactCompareImage extends React.Component {
         width: '100%',
       },
       overImage: {
-        clip: `rect(auto, ${this.state.imageWidth * this.state.sliderPositionPercentage}px, auto, auto)`,
+        clip: `rect(auto, ${this.state.imageWidth *
+          this.state.sliderPositionPercentage}px, auto, auto)`,
         display: 'block',
         height: '100%', // fit to the height of under image
         objectFit: 'cover', // protrudes is hidden
@@ -135,7 +128,10 @@ class ReactCompareImage extends React.Component {
         backgroundColor: 'white',
         cursor: 'ew-resize',
         height: '100%',
-        left:this.state.imageWidth * this.state.sliderPositionPercentage -this.props.sliderWidth / 2 +          'px',
+        left:
+          this.state.imageWidth * this.state.sliderPositionPercentage -
+          this.props.sliderWidth / 2 +
+          'px',
         position: 'absolute',
         top: 0,
         width: `${this.props.sliderWidth}px`,
@@ -144,11 +140,18 @@ class ReactCompareImage extends React.Component {
     };
 
     return (
-      <div style={styles.container} ref="container">
+      <div
+        style={styles.container}
+        ref={ref => {
+          this.containerRef = ref;
+        }}
+      >
         <img
           alt="left"
           className="img-comp-under"
-          ref="underImage"
+          ref={ref => {
+            this.underImageRef = ref;
+          }}
           src={this.props.rightImage}
           style={styles.underImage}
         />
@@ -161,7 +164,7 @@ class ReactCompareImage extends React.Component {
         <div className="img-comp-slider" style={styles.slider} />
       </div>
     );
-  }
+  };
 }
 
 ReactCompareImage.propTypes = propTypes;
