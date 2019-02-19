279cea7c173aa8d857e8626af42e0a7a124a5fb0
tasninAlam <tasnimalamcse@gmail.com>
2019-02-19 21:41:21 +0600

    Remove handle color

--- src/ReactCompareImage.js
+++ src/ReactCompareImage.js
@@ -244,9 +244,7 @@ class ReactCompareImage extends React.Component {
       },
       handle: {
         alignItems: 'center',
-        border: `${this.props.sliderLineWidth}px solid ${
-          this.props.sliderLineColor
-        }`,
+        border: `${this.props.sliderLineWidth}px solid white`,
         borderRadius: '100%',
         boxShadow:
           '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)',
