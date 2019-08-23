81caaf0d2c50d53efd100c0ff134047963fc54c0
Shota Tamura <tamura@yuuniworks.com>
2019-08-23 09:52:26 +0900

    upgrade packages

--- src/ReactCompareImage.test.jsx
+++ src/ReactCompareImage.test.jsx
@@ -1,5 +1,5 @@
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import React from 'react';
-import { cleanup, fireEvent, render } from 'react-testing-library';
 import ReactCompareImage from './ReactCompareImage';
 
 /**
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,3 @@
 /* eslint-disable */
+import '@testing-library/jest-dom/extend-expect';
 import 'babel-polyfill'; // これがないとasync/awaitを書いた時にコケる
-import 'jest-dom/extend-expect';
