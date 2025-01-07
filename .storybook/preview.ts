8f6482f489e0dd8a0d7f8e31352841d970c3f605
Shota Tamura <tamura@yuuniworks.com>
2025-01-07 12:43:25 +0900

    chore: migrate storybook v6 to v8

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,11 @@ import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
+    options: {
+      storySort: {
+        order: ['ReactCompareImage', ['Basic', 'Advanced']],
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
